76b5235654861d3424ac6cc432f37470cc1619e4
Carlos Eduardo Artioli <caduartioli@gmail.com>
2022-11-14 23:12:58 -0300

    Conteinerizando a Stack de Microsserviços com Docker e Docker Compose

--- cambio-service/src/main/java/br/com/caduartioli/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/br/com/caduartioli/cambioservice/controller/CambioController.java
@@ -4,6 +4,8 @@ import br.com.caduartioli.cambioservice.model.Cambio;
 import br.com.caduartioli.cambioservice.repository.CambioRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,8 @@ import java.math.RoundingMode;
 @RequestMapping("cambio-service")
 public class CambioController {
 
+    private Logger logger = LoggerFactory.getLogger(CambioController.class);
+
     @Autowired
     private Environment environment;
 
@@ -32,6 +36,8 @@ public class CambioController {
             @PathVariable("from") String from,
             @PathVariable("to") String to
             ) {
+
+        logger.info("getCambio is called with -> {}, {} and {}", amount, from, to);
 
         var cambio = repository.findByFromAndTo(from, to);
         if (cambio == null) throw new RuntimeException("Currency Unsupported");
